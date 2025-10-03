b3756cdbb1a5bdaad2e189cc6fd24947dabf1f08
Alexander <alexgo1992@gmail.com>
2025-10-03 16:10:58 +0300

    первые задания

---
+++ task_2.py
@@ -0,0 +1,26 @@
+#todo: Преобразуйте переменную age и foo в число
+age = "23"
+foo = "23abc"
+age = int(age)
+foo = int(foo)
+
+#Преобразуйте переменную age в Boolean
+age = "123abc"
+age = bool(age)
+
+#Преобразуйте переменную flag в Boolean
+flag = 1
+flag = bool(flag)
+
+#Преобразуйте значение в Boolean
+str_one = "Privet"
+str_two = ""
+str_one = bool(str_one)
+str_two = bool(str_two)
+
+#Преобразуйте значение 0 и 1 в Boolean
+bool(0)
+bool(1)
+
+#Преобразуйте False в строку
+str(False)
