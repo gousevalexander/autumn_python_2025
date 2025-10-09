0063c3c9a49854560abf589b7472c43e7d6f078c
Alexander <alexgo1992@gmail.com>
2025-10-09 20:41:36 +0300

    доработан task_13

--- task_13.py
+++ task_13.py
@@ -5,46 +5,23 @@
 # если 1984 год был началом цикла — годом зеленой крысы.
 
 starting_year = 1984
-year = int(input('Введите номер года: '))
-animal_number = abs(year - starting_year) % 12
-color_number = abs(year - starting_year) % 60
-
-match animal_number:
-    case 0:
-        animal = 'крысы'
-    case 1:
-        animal = 'коровы'
-    case 2:
-        animal = 'тигра'
-    case 3:
-        animal = 'зайца'
-    case 4:
-        animal = 'дракона'
-    case 5:
-        animal = 'змеи'
-    case 6:
-        animal = 'лошади'
-    case 7:
-        animal = 'овцы'
-    case 8:
-        animal = 'обезьяны'
-    case 9:
-        animal = 'курицы'
-    case 10:
-        animal = 'собаки'
-    case 11:
-        animal = 'свиньи'
+year = input('Введите номер года: ')
+color_number = int(year[-1])
+year = int(year)
+animal_number = (year + 8888) % 12
+animals = [ 'крысы', 'коровы', 'тигра', 'зайца', 'дракона', 'змеи', 'лошади', 'овцы', 'обезьяны', 'курицы', 'собаки',
+           'свиньи' ]
 
 match color_number:
-    case num if 0 <= num <= 11:
-        color = ('зелёного', 'зелёной')
-    case num if 12 <= num <= 23:
-        color = ('красного', 'красной')
-    case num if 24 <= num <= 35:
-        color = ('жёлтого', 'жёлтой')
-    case num if 36 <= num <= 47:
+    case 0|1:
         color = ('белого', 'белой')
-    case num if 48 <= num <= 59:
+    case 2|3:
         color = ('чёрного', 'чёрной')
+    case 4|5:
+        color = ('зелёного', 'зелёной')
+    case 6|7:
+        color = ('красного', 'красной')
+    case 8|9:
+        color = ('жёлтого', 'жёлтой')
 
-print(f'Год {color[animal_number in [0, 1, 5, 6, 7, 8, 9, 10, 11]]} {animal}')
+print(f'Год {color[animal_number in [0, 1, 5, 6, 7, 8, 9, 10, 11]]} {animals[animal_number]}')
